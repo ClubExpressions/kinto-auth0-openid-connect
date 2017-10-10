1151a5a68249a1b367b6707c625b0ac993f1e186
Damien Lecan <dev@dlecan.com>
2017-10-10 21:53:22 +0200

    JKWS requests are now cached and refreshed twice a day

--- kinto_auth0_openid_connect/authentication.py
+++ kinto_auth0_openid_connect/authentication.py
@@ -21,6 +21,7 @@ class Auth0OIDCAuthenticationPolicy(CallbackAuthenticationPolicy):
     def __init__(self, realm='Realm'):
         print("Init Auth0OIDCAuthenticationPolicy")
         self.realm = realm
+        self._cache = None
 
     def unauthenticated_userid(self, request):
         """Return the Auth0 userid or ``None`` if token could not be verified.
@@ -49,21 +50,7 @@ class Auth0OIDCAuthenticationPolicy(CallbackAuthenticationPolicy):
                                      "description": "Unable to parse authentication"
                                                     "token."}, 400)
 
-        jwks_resp = requests.get("https://" + auth_domain + "/.well-known/jwks.json")
-        jwks = jwks_resp.json()
-        unverified_header = jwt.get_unverified_header(token)
-        rsa_key = {}
-        algorithm = None
-        for key in jwks["keys"]:
-            if key["kid"] == unverified_header["kid"]:
-                rsa_key = {
-                    "kty": key["kty"],
-                    "kid": key["kid"],
-                    "use": key["use"],
-                    "n": key["n"],
-                    "e": key["e"]
-                }
-                algorithm = key["alg"]
+        (rsa_key, algorithm) = self._get_crypto_data(request, auth_domain, token)
         if rsa_key:
             try:
                 profile = jwt.decode(
@@ -90,3 +77,42 @@ class Auth0OIDCAuthenticationPolicy(CallbackAuthenticationPolicy):
             return profile["sub"]
         return handle_error({"code": "invalid_header",
                              "description": "Unable to find appropriate key"}, 400)
+
+    def _get_cache(self, request):
+      """Instantiate cache when first request comes in.
+      """
+      if self._cache is None:
+        if hasattr(request.registry, 'cache'):
+          self._cache = request.registry.cache
+
+      return self._cache
+
+    def _get_crypto_data(self, request, auth_domain, token):
+
+      cache = self._get_cache(request)
+
+      unverified_header = jwt.get_unverified_header(token)
+      unverified_kid = unverified_header["kid"]
+
+      resp = cache.get(unverified_kid)
+
+      if resp is None:
+        jwks_resp = requests.get("https://" + auth_domain + "/.well-known/jwks.json")
+        jwks = jwks_resp.json()
+        rsa_key = {}
+        algorithm = None
+
+        for key in jwks["keys"]:
+          if key["kid"] == unverified_kid:
+            rsa_key = {
+              "kty": key["kty"],
+              "kid": key["kid"],
+              "use": key["use"],
+              "n": key["n"],
+              "e": key["e"]
+            }
+            algorithm = key["alg"]
+        resp = (rsa_key, algorithm)
+        cache.set(unverified_kid, resp, 43200)
+
+      return resp
